bccf0b98f33c8f976db7513118ec7ff52f6b4a40
mika <345249887@qq.com>
2020-06-03 09:05:19 +0800

    1

--- app/src/main/java/com/hym/appstore/ui/fragment/RecommendFragment.java
+++ app/src/main/java/com/hym/appstore/ui/fragment/RecommendFragment.java
@@ -2,9 +2,7 @@ package com.hym.appstore.ui.fragment;
 
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +12,10 @@ import com.hym.appstore.R;
 import com.hym.appstore.bean.RecommendBean;
 import com.hym.appstore.nohttp.CallServer;
 import com.hym.appstore.ui.adapter.RecommendRVAdapter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.yanzhenjie.nohttp.NoHttp;
 import com.yanzhenjie.nohttp.RequestMethod;
 import com.yanzhenjie.nohttp.rest.Request;
@@ -31,6 +33,8 @@ public class RecommendFragment extends BaseFragment {
 
     @BindView(R.id.recommend_rv)
     RecyclerView mRecommendRv;
+    @BindView(R.id.recommend_refreshLayout)
+    RefreshLayout recommendRefreshLayout;
 
     private Request<String> recommendRequest;
     /**
@@ -64,7 +68,13 @@ public class RecommendFragment extends BaseFragment {
 
     @Override
     protected void initEvent() {
-
+        recommendRefreshLayout.setRefreshHeader(new ClassicsHeader(getActivity()));//设置Header
+        recommendRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                CallServer.getInstance().add(getActivity(), 0, recommendRequest, RecommendFragment.this, true, true);
+            }
+        });
     }
 
     @Override
@@ -76,12 +86,12 @@ public class RecommendFragment extends BaseFragment {
                 Log.d("onSucceed", response.get());
                 RecommendBean recommendBean = mGson.fromJson(response.get(), RecommendBean.class);
                 List<RecommendBean.DataBean.ItemsBean> items = recommendBean.getData().getItems();
+                mGamelist.clear();
                 mGamelist.addAll(items);
                 layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
                 mRecommendRv.setLayoutManager(layoutManager);
                 RecommendRVAdapter recommendRVAdapter = new RecommendRVAdapter(mGamelist, getActivity());
                 mRecommendRv.setAdapter(recommendRVAdapter);
-//                mRecommendRv.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
                 // 设置数据后就要给RecyclerView设置点击事件
                 recommendRVAdapter.setOnItemClickListener(new RecommendRVAdapter.ItemClickListener() {
                     @Override
@@ -90,49 +100,10 @@ public class RecommendFragment extends BaseFragment {
                         ToastUtils.show(mGamelist.get(position).getApp_name());
                     }
                 });
-               /* mHomeRecommend.setVisibility(View.VISIBLE);
-                GoodsInfoBean goodsInfo = mGson.fromJson(response.get(), GoodsInfoBean.class);
-//
-                List<GoodsInfoBean.GoodlistBean> goodsList = goodsInfo.getGoodlist();
-                mGoodslist.clear();
-                mGoodslist.addAll(goodsList);
-                layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
-                mHomeRv.setLayoutManager(layoutManager);
-                HomeGoodsAdapter homeGoodsAdapter = new HomeGoodsAdapter(mGoodslist, getActivity());
-                mHomeRv.setAdapter(homeGoodsAdapter);
-                mHomeRv.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
-                // 设置数据后就要给RecyclerView设置点击事件
-                homeGoodsAdapter.setOnItemClickListener(new HomeGoodsAdapter.ItemClickListener() {
-                    @Override
-                    public void onItemClick(int position) {
-                        // 这里本来是跳转页面 ，我们就在这里直接让其弹toast来演示
-                        Toast.makeText(getActivity() , mGoodslist.get(position).getShort_title() , Toast.LENGTH_SHORT).show();
-                    }
-                });*/
                 break;
-            case 1:
-               /* mHomeFilm.setVisibility(View.VISIBLE);
-                Gson gson1 = new Gson();
-                HomeFilmBean homeFilmBean = gson1.fromJson(response.get(), HomeFilmBean.class);
-                Log.d("onSucceed", response.get());
-                List<HomeFilmBean.ResultBean> result = homeFilmBean.getResult();
-                mFilmlist.clear();
-                mFilmlist.addAll(result);
-                layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
-                mHomeFilmRv.setLayoutManager(layoutManager);
-                HomeFilmAdapter homeFilmAdapter = new HomeFilmAdapter(mFilmlist, getActivity());
-                mHomeFilmRv.setAdapter(homeFilmAdapter);
-                // 设置数据后就要给RecyclerView设置点击事件
-                homeFilmAdapter.setOnItemClickListener(new HomeFilmAdapter.ItemClickListener() {
-                    @Override
-                    public void onItemClick(int position) {
-                        // 这里本来是跳转页面 ，我们就在这里直接让其弹toast来演示
-                        Toast.makeText(getActivity() , mFilmlist.get(position).getFilmName() , Toast.LENGTH_SHORT).show();
-                    }
-                });*/
-                break;
+
         }
-//        mHomeRefreshLayout.finishRefresh();//结束刷新
+        recommendRefreshLayout.finishRefresh();//结束刷新
     }
 
     @Override
@@ -144,7 +115,7 @@ public class RecommendFragment extends BaseFragment {
     /**
      * 定义RecyclerView选项单击事件的回调接口
      */
-    public interface OnItemClickListener{
+    public interface OnItemClickListener {
         //也可以不在这个activity或者是fragment中来声明接口，可以在项目中单独创建一个interface，就改成static就OK
         //参数（父组件，当前单击的View,单击的View的位置，数据）
         void onItemClick(RecyclerView parent, View view, int position, Map data);
