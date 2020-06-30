b2e7accef9ecbde8159306a976821071b3f78b3e
mika <345249887@qq.com>
2020-06-30 08:53:17 +0800

    app详情

--- app/src/main/java/com/hym/appstore/ui/activity/AppDetailsActivity.java
+++ app/src/main/java/com/hym/appstore/ui/activity/AppDetailsActivity.java
@@ -4,11 +4,17 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -19,6 +25,7 @@ import com.hym.appstore.dagger2.component.AppComponent;
 import com.hym.appstore.ui.fragment.AppDetailFragment;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AppDetailsActivity extends BaseActivity {
 
@@ -29,10 +36,15 @@ public class AppDetailsActivity extends BaseActivity {
     ImageView imgIcon;
     @BindView(R.id.txt_name)
     TextView txtName;
+    @BindView(R.id.view_temp)
+    View viewTemp;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.view_coordinator)
+    CoordinatorLayout viewCoordinator;
+
 
     private AppInfoBean mAppInfoBean;
-
-
 
 
     @Override
@@ -51,10 +63,10 @@ public class AppDetailsActivity extends BaseActivity {
         mAppInfoBean = (AppInfoBean) getIntent().getSerializableExtra("appInfo");
         initFragment();
 
-      /*  View view = mMyApplication.getView();
+        View view = mMyApplication.getView();
         Bitmap bitmap = getViewImageCache(view);
         if (bitmap != null) {
-            frameLayout.setBackground(new BitmapDrawable(bitmap));
+            viewTemp.setBackground(new BitmapDrawable(bitmap));
         }
 
         int[] location = new int[2];
@@ -62,7 +74,7 @@ public class AppDetailsActivity extends BaseActivity {
         int left = location[0];
         int top = location[1];
 
-        ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(frameLayout.getLayoutParams());
+        ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(viewTemp.getLayoutParams());
         marginLayoutParams.topMargin = top;
         marginLayoutParams.leftMargin = left;
         marginLayoutParams.width = view.getWidth();
@@ -70,9 +82,9 @@ public class AppDetailsActivity extends BaseActivity {
 
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(marginLayoutParams);
-        frameLayout.setLayoutParams(params);
+        viewTemp.setLayoutParams(params);
 
-        open();*/
+        open();
 
     }
 
@@ -104,31 +116,34 @@ public class AppDetailsActivity extends BaseActivity {
     }
 
 
-    private void open(){
+    private void open() {
         int h = DensityUtil.getScreenH(this);
-        ObjectAnimator animator = ObjectAnimator.ofFloat(frameLayout, "scaleY", 1f, (float) h);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(viewTemp, "scaleY", 1f, (float) h);
         animator.setStartDelay(500);
-//        animator.setDuration(500);
+        animator.setDuration(2000);
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                frameLayout.setVisibility(View.GONE);
+                viewTemp.setVisibility(View.GONE);
+                viewCoordinator.setVisibility(View.VISIBLE);
                 initFragment();
             }
 
             @Override
             public void onAnimationStart(Animator animation) {
-                frameLayout.setBackgroundColor(getResources().getColor(R.color.theme_while));
+                viewTemp.setBackgroundColor(getResources().getColor(R.color.theme_while));
             }
         });
         animator.start();
     }
 
-    private void initFragment(){
+    private void initFragment() {
         AppDetailFragment fragment = new AppDetailFragment(mAppInfoBean.getId());
         FragmentManager supportFragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.app_details_fl,fragment);
+        fragmentTransaction.add(R.id.app_details_fl, fragment);
         fragmentTransaction.commitAllowingStateLoss();
     }
+
+
 }
