617ea24ed8d7fafaf920ba2eba84735d1ebe60fb
mika <345249887@qq.com>
2020-07-24 08:09:28 +0800

    主题详情

--- app/src/main/java/com/hym/appstore/ui/fragment/SubjectDetailFragment.java
+++ app/src/main/java/com/hym/appstore/ui/fragment/SubjectDetailFragment.java
@@ -1,30 +1,84 @@
 package com.hym.appstore.ui.fragment;
 
-import com.hym.appstore.dagger2.component.AppComponent;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hym.appstore.R;
+import com.hym.appstore.bean.Subject;
+import com.hym.appstore.bean.SubjectDetail;
+import com.hym.appstore.common.Constant;
+import com.hym.appstore.common.imageloader.ImageLoader;
+import com.hym.appstore.ui.adapter.AppInfoAdapter;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import zlc.season.rxdownload2.RxDownload;
 
 public class SubjectDetailFragment extends BaseSubjectFragment {
+
+    @BindView(R.id.imageview)
+    ImageView mImageView;
+    @BindView(R.id.txt_desc)
+    TextView mtxtDesc;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    @Inject
+    RxDownload mRxDownload;
+
+
+    private Subject mSubject;
+
+    private AppInfoAdapter mAdapter;
+
+    public SubjectDetailFragment() {
+
+    }
+
+    public SubjectDetailFragment setArgs(Subject subject) {
+        this.mSubject = subject;
+        return this;
+    }
+
+
     @Override
     protected int setLayoutResourceID() {
-        return 0;
+        return R.layout.fragment_subject_detail;
     }
 
-    @Override
-    protected void setupActivityComponent(AppComponent appComponent) {
-
-    }
 
     @Override
     protected void init() {
+        initRecycleView();
+        mPresenter.getSubjectDetail(mSubject.getRelatedId());
+    }
+
+    private void initRecycleView() {
+        mAdapter = AppInfoAdapter.builder().showBrief(false).showCategoryName(true)
+                .rxDownload(mRxDownload).build();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
+
+        mRecyclerView.addItemDecoration(itemDecoration);
+
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 
     @Override
-    protected void initView() {
+    public void showSubjectDetail(SubjectDetail detail) {
+        ImageLoader.load(Constant.BASE_IMG_URL + detail.getPhoneBigIcon(), mImageView);
 
-    }
+        mtxtDesc.setText(detail.getDescription());
 
-    @Override
-    protected void initEvent() {
-
+        mAdapter.addData(detail.getListApp());
     }
 }
